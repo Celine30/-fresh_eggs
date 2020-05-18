4720c62dbbbbcc88e6bb606ca4b340907debb0a0
Celine <celi.caurier@gmail.com>
2020-05-18 20:27:38 +0200

    DatePicker Filter and Max_Mini

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder , FormArray} from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  ResaForm : FormGroup;
+  selected :string;
+  minDate: Date;
+  maxDate: Date;  
+ 
+  myFilter = (d: Date | null): boolean => {
+    const day = (d || new Date()).getDay();
+    return day !== 0 && day !== 2 && day !== 3 && day !== 4 &&  day !== 6;
+  }
+
+  constructor(private formBuilder : FormBuilder) { 
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date();
+    this.maxDate = new Date();
+    this.maxDate.setDate(this.minDate.getDate()+6);
+  }
+
+  initForm(){
+    this.ResaForm = this.formBuilder.group({
+      name: '',
+      last_name:'',
+      number:'',
+      date:'',
+      boite:'',
+      
+
+    })
+  }
+
+  ngOnInit(): void {
+    this.initForm()
+  }
+  onSubmitForm(){
+    const formValue = this.ResaForm.value;
+    console.log(formValue)
+    var ladate=new Date()
+    console.log("ladate.getDay() = "+ladate.getDay()+"<BR>")
+    var tab_jour=new Array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
+    console.log("Nous sommes un "+tab_jour[ladate.getDay()])
+  }
+
+}
---
+++ src/app/material.module.ts
@@ -0,0 +1,39 @@
+import {NgModule} from '@angular/core';
+import {MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+
+import {MatTableModule} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+
+@NgModule({
+    exports: [
+        MatSliderModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSlideToggleModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
+        MatTableModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule 
+    ]
+})
+
+export class MaterialModule {
+}
