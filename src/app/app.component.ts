eda50502404a1c382b7f872970fd508da099dbc7
Celine <celi.caurier@gmail.com>
2020-05-19 21:18:27 +0200

    .......

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
   title = 'fresh-eggs';
   opened = false;
-  
+  auth:boolean;
+
+  authSubscription: Subscription;
+
+  constructor(private authservice : AuthService) { }
+
+  ngOnInit(): void {
+    this.authSubscription = this.authservice.authSubject.subscribe(
+      (data:boolean)=>{
+        this.auth = data
+      }
+    );
+    this.authservice.emitPostSubject()
+  } 
 }
+
+
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder , FormArray} from '@angular/forms';
+import { FormGroup, FormBuilder , FormArray, Validators, FormControl} from '@angular/forms';
+import { OrderService } from '../services/order.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
+
+  name = new FormControl('');
+  
+  email = new FormControl('');
 
   ResaForm : FormGroup;
   selected :string;
   minDate: Date;
   maxDate: Date;  
- 
+
+
   myFilter = (d: Date | null): boolean => {
     const day = (d || new Date()).getDay();
     return day !== 0 && day !== 2 && day !== 3 && day !== 4 &&  day !== 6;
   }
 
-  constructor(private formBuilder : FormBuilder) { 
+  
+
+  constructor(private orderservice:OrderService, private formBuilder : FormBuilder) { 
     const currentYear = new Date().getFullYear();
     this.minDate = new Date();
     this.maxDate = new Date();
@@ -27,24 +37,25 @@ export class HomeComponent implements OnInit {
 
   initForm(){
     this.ResaForm = this.formBuilder.group({
-      name: '',
-      last_name:'',
-      number:'',
-      date:'',
-      boite:'',
+      name: ['', Validators.required],
+      last_name:['', Validators.required],
+      number:['', Validators.required],
+      date:['', Validators.required],
+      boite:['', Validators.required],
     })
+
   }
 
   ngOnInit(): void {
-    this.initForm()
+    this.initForm();
   }
+
+
+
   onSubmitForm(){
     const formValue = this.ResaForm.value;
-    console.log(formValue)
-    var ladate=new Date()
-    console.log("ladate.getDay() = "+ladate.getDay()+"<BR>")
-    var tab_jour=new Array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
-    console.log("Nous sommes un "+tab_jour[ladate.getDay()])
+    this.orderservice.addOrder(formValue);
+    
   }
 
 }
