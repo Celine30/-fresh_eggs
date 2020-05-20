6e10a20490e81736e9e128dd87800e8e7c7a0502
Celine <celi.caurier@gmail.com>
2020-05-20 21:42:21 +0200

    Objet Order

---
+++ src/app/model/order.ts
@@ -0,0 +1,10 @@
+export class Order {
+
+    constructor(
+        public id :number , 
+        public name : string ,
+        public first_name: string, 
+        public number: string, 
+        public date : string , 
+        public boite : string) {}
+}
