7456a92d72a12d4c91bf90c170dc57604fdcd1f7
Celine <celi.caurier@gmail.com>
2020-05-20 21:42:51 +0200

    Form Validate

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder , FormArray, Validators, FormControl} from '@angular/forms';
+import { FormGroup, FormBuilder , FormArray, Validators, FormControl, ValidatorFn, AbstractControl} from '@angular/forms';
 import { OrderService } from '../services/order.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -11,24 +12,18 @@ import { OrderService } from '../services/order.service';
 
 export class HomeComponent implements OnInit {
 
-  name = new FormControl('');
-  
-  email = new FormControl('');
-
   ResaForm : FormGroup;
   selected :string;
   minDate: Date;
   maxDate: Date;  
-
+  message:string;
 
   myFilter = (d: Date | null): boolean => {
     const day = (d || new Date()).getDay();
     return day !== 0 && day !== 2 && day !== 3 && day !== 4 &&  day !== 6;
   }
 
-  
-
-  constructor(private orderservice:OrderService, private formBuilder : FormBuilder) { 
+  constructor(private orderservice:OrderService, private formBuilder : FormBuilder , private Router :Router, private route: ActivatedRoute) { 
     const currentYear = new Date().getFullYear();
     this.minDate = new Date();
     this.maxDate = new Date();
@@ -37,25 +32,38 @@ export class HomeComponent implements OnInit {
 
   initForm(){
     this.ResaForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      last_name:['', Validators.required],
-      number:['', Validators.required],
-      date:['', Validators.required],
-      boite:['', Validators.required],
+      name:['',[Validators.required]],
+      last_name:['',[Validators.required]],
+      number:['',[Validators.required , Validators.pattern('^(0[0-9]{1})([/ _.-]?[0-9]{2}){4}')]],
+      date:['',[Validators.required]],
+      boite:['',[Validators.required]],
+      engagement:['',[Validators.required]],
     })
-
   }
 
   ngOnInit(): void {
     this.initForm();
+
+    this.message = this.route.snapshot.params['caller']
   }
 
-
+  get f() { return this.ResaForm.controls; }
 
   onSubmitForm(){
     const formValue = this.ResaForm.value;
-    this.orderservice.addOrder(formValue);
-    
+    console.log(formValue)
+    this.orderservice.addOrder(formValue); 
+    this.Router.navigate(["home", 'send']);
+  }
+
+  onReset(): void {
+    this.resetForm();
+  }
+
+  resetForm(value: any = undefined): void {
+    this.ResaForm.reset(value);
   }
 
 }
+
+
